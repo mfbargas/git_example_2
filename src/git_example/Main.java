61f8953b09eb1fcb9e89dd3bdf087a8c48084b7b
Manuel Fragoso <fragoso.manuel@gmail.com>
2019-07-21 22:36:10 -0500

    version 3

--- src/git_example/Main.java
+++ src/git_example/Main.java
@@ -7,6 +7,8 @@ public class Main {
 		System.out.println("Esta es la version inicial de mi codigo");
 		
 		System.out.println("Esta es un nueva linea de cogido de mi programa");
+		
+		System.out.println("Esta es tercera linea de cogido de mi programa");
 
 	}
 
